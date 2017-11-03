3a105aa6c249a894b117e4606a84f30e8899046e
Ryan Kessler <ryan@ryans-mbp-3.devices.brown.edu>
2017-11-03 14:43:19 -0400

    Refines handling of pages to include WY

--- src/data/extract.py
+++ src/data/extract.py
@@ -19,20 +19,28 @@ def parse_ui_sig_prov_page(df):
     
     return(ui_policy)
 
-def parse_ui_sig_prov_pdf(month_year):
+def parse_ui_sig_prov_pdf(month_year, pages = 6):
     
     ui_policy_pages = []
-    for page in range(1, 5):
+    
+    for page in range(1, pages):
+
         df_page = read_pdf(f"../../data/interim/{month_year}.pdf",
                            pages          = str(page),
                            spreadsheet    = True,
                            pandas_options = {'header': 11})
-
-        ui_policy_page = parse_ui_sig_prov_page(df_page)
+        try:
+            ui_policy_page = parse_ui_sig_prov_page(df_page)
+        except Exception as e:
+            if str(e) == "'NoneType' object has no attribute 'iloc'":
+                break 
+            else: 
+                raise        
         ui_policy_page['month_year'] = month_year
         ui_policy_pages.append(ui_policy_page)
     
     ui_policy_pdf = pd.concat(ui_policy_pages)
+
     return(ui_policy_pdf)
 
 def parse_ui_sig_provs(months, years):
@@ -41,12 +49,14 @@ def parse_ui_sig_provs(months, years):
         for month in months:
             month_year = f"{month}{year}"
             print(f"Parsing {month_year}...")
+
             try:
-                month_year_data = parse_ui_sig_prov_pdf(month_year)
-                month_year_data.to_csv(f"../../data/interim/{month_year}.csv", index = False)
+                month_year_data = parse_ui_sig_prov_pdf(month_year = month_year)
             except Exception as e:
                 print(f"Failed on {month_year}")
                 print(f"{e}")
+
+            month_year_data.to_csv(f"../../data/interim/{month_year}.csv", index = False)
 
 months = ['January', 'July']
 years  = list(range(2006, 2018))
